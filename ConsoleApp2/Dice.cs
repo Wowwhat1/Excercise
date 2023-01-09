135ffb9f49a85953a6f9eefedaecd3c1f92eeb77
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-01-09 15:37:47 +0700

    first commit

---
+++ ConsoleApp2/Dice.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2;
+
+public class Dice
+{
+    // Field
+    private int _side;
+    // Property
+    public int Side
+    {
+        get { return _side; }
+        set { if (_side > 0) _side = value;}
+    }
+
+    public int Roll()
+    {
+        Random rd = new Random();
+        return rd.Next(1, _side + 1);
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+Dice dice = new Dice();
+Console.WriteLine("Enter number of side");
+dice.Side = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your expected number: ");
+int expectedNum = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your expected number is {expectedNum}, the real result is: {dice.Roll()}");
+Console.ReadLine();
